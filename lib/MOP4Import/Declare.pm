496ed7c21dd010b50fa4e45ab3a5d4a6fa57ad48
KOBAYASI, Hiroaki <hkoba@ssri.com>
2014-06-14 09:32:13 +0900

    initial commit

---
+++ lib/MOP4Import/Declare.pm
@@ -0,0 +1,32 @@
+package MOP4Import::Declare;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MOP4Import::Declare -
+
+=head1 SYNOPSIS
+
+  use MOP4Import::Declare;
+
+=head1 DESCRIPTION
+
+MOP4Import::Declare is
+
+=head1 AUTHOR
+
+KOBAYASHI, Hiroaki E<lt>hkoba@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
