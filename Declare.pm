db650834875fd234ebf607cd93698ed533e1090a
KOBAYASI, Hiroaki <buribullet@gmail.com>
2015-11-01 22:02:48 +0900

    Fix dist_name

--- Declare.pm
+++ Declare.pm
@@ -3,7 +3,7 @@ package MOP4Import::Declare;
 use 5.010;
 use strict;
 use warnings qw(FATAL all NONFATAL misc);
-our $VERSION = '0.001_001';
+our $VERSION = '0.001_002';
 use Carp;
 use mro qw/c3/;
 
