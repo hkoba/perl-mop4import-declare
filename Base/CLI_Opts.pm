7275fcd170026815990be1b2ad924ec794a20edc
Makamaka <makamaka.donzoko@gmail.com>
2017-08-31 17:36:12 +0900

    Make more Getopt::Long-tick

--- Base/CLI_Opts.pm
+++ Base/CLI_Opts.pm
@@ -3,6 +3,7 @@ use MOP4Import::Base::CLI -as_base
 #  , [extend => FieldSpec => qw/type alias/]
 ;
 use MOP4Import::Opts;
+use Carp ();
 use Data::Dumper;
 
 use constant DEBUG => $ENV{DEBUG_MOP4IMPORT};
@@ -18,7 +19,6 @@ sub declare_options {
         my $o = ref $_ ? $_ : [$_];
 
         unless ( $o->[0] =~ /([-\w]+)(?:\|([-\w]+))?(?:=([is]@?))?/ ) {
-            require Carp;
             Carp::croak("Invalid option format - " . $o->[0]);
         }
 
@@ -57,21 +57,38 @@ sub parse_opts {
             }
         }
     }
-
+    print STDERR "option format: ", Data::Dumper->new([$form])->Dump, "\n" if DEBUG;
     my $key = 1;
     my @stack;
     my $cmd;
     for my $arg ( @$list ) {
-        print "`$arg`\n";
         if ( $key ) {
-            if ( $arg =~ /^--?([^=]+)=(.+)$/ ) {
+            if ( $arg =~ /^-([^=]+)=(.+)$/ ) { # -f=foo
                 push @stack, $arg;
             }
-            elsif ( !$cmd && $arg =~ /^--?([^=]+)/ ) {
-                if ( not defined $form->{$1} ) {
-                    ...
+            elsif ( $arg =~ /^--([^=]+)=(.+)$/ ) { # --foo=baz
+                push @stack, $arg;
+            }
+            elsif ( !$cmd && $arg =~ /^-([^-=]+)/ ) { # -fb( value)
+                my $myaby_opts = $1;
+                for my $s (split//, $myaby_opts) {
+                    if ( not defined $form->{$s} ) {
+                        Carp::croak("Invalid option format - " . $arg);
+                    }
+                    if ( $form->{$s}->{type} ne 'flag' ) {
+                        push @stack, '-' . $s;
+                        $key = 0;
+                    }
+                    else {
+                        push @stack, '-' . $s;
+                    }
                 }
-                elsif ( $form->{$1}->{type} ne 'flag' ) {
+            }
+            elsif ( !$cmd && $arg =~ /^--([^=]+)/ ) { # --bar( value)
+                if ( not defined $form->{_hyphen2underscore($1)} ) {
+                    Carp::croak("Invalid option format - " . $arg);
+                }
+                if ( $form->{_hyphen2underscore($1)}->{type} ne 'flag' ) {
                     push @stack, $arg;
                     $key = 0;
                 }
@@ -79,23 +96,64 @@ sub parse_opts {
                     push @stack, $arg;
                 }
             }
-            else {
+            else { # perhaps this is command!
                 $cmd = $arg;
                 push @stack, $arg;
             }
         }
-        else {
+        else { # change to Base::CLI aware format
             $stack[-1] .= '=' . $arg;
             $key = 1;
         }
     }
-#    print Dumper($list);
-#    print Dumper(\@stack);
-
+    #print Dumper($list);
+    #print Dumper(\@stack);
     @$list = @stack;
 
     $class->SUPER::parse_opts($list, $result, \%alias, @rest);
 }
+
+
+sub configure {
+    my ( $self, @args ) = @_;
+    my $fields = MOP4Import::Declare::fields_hash($self);
+    my @res;
+    my %map;
+    #print Dumper([@args]);
+    while ( defined(my $name = shift @args) ) {
+        my $type = $fields->{$name}->{type};
+        my $val  = shift(@args);
+
+        if ( $type =~ /(.)@/ ) {
+            if ( $1 eq 'i' ) {
+                if ( $val !~ /^[0-9]+$/ ) {
+                    Carp::croak("option `$name` takes integer.");
+                }
+            }
+
+            if (not exists $map{$name}) {
+                push @res, $name;
+                push @res, [$val];
+                $map{$name} = $#res;
+            }
+            else {
+                push @{ $res[ $map{$name} ] }, $val;
+            }
+            next;
+        }
+        elsif ( $type eq 'i' ) {
+            if ( $val !~ /^[0-9]+$/ ) {
+                Carp::croak("option `$name` takes integer.");
+            }
+        }
+
+        push @res, $name;
+        push @res, $val;
+    }
+    #print Dumper([@res]);
+    $self->SUPER::configure(@res);
+}
+
 
 sub run {
     my ($class, $arglist) = @_;
@@ -104,16 +162,22 @@ sub run {
     # $class->new する
     my $obj = $class->new(@opts);
     # 次の引数を取り出して、サブコマンドとして解釈を試みる
-    my $cmd = shift @$arglist || "help";
+    my $cmd = shift @$arglist || "default";
     # サブコマンド毎の処理を行う
+    # 結果を何らかの形式で出力する
+    # 望ましい終了コードを返す（差し当たり、必要ならば各メソッド内で指定する）
     if (my $sub = $obj->can("cmd_$cmd")) {
         $sub->($obj, @$arglist);
     } else {
         print STDERR "Cmd `$cmd` is not implemented.\n";
         exit(1);
     }
-    # 結果を何らかの形式で出力する
-    # 望ましい終了コードを返す
 }
+
+sub _hyphen2underscore {
+    my ($v) = @_;
+    $v =~ tr/-/_/r;
+}
+
 
 1;
