c53b0c9d41a42cd58d785a96da13840875ba1d6f
Kobayasi, Hiroaki <buribullet@gmail.com>
2018-02-13 16:16:19 +0900

    For #34 - Add note for future mod.

--- Types.pm
+++ Types.pm
@@ -5,6 +5,7 @@ use warnings qw(FATAL all NONFATAL misc);
 use Carp;
 
 use MOP4Import::Pairs -as_base, qw/Opts m4i_opts/;
+# XXX: [carp_not => MOP4Import::Util]
 use MOP4Import::Declare::Type -as_base;
 use MOP4Import::Util;
 
