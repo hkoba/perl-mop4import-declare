4a0248c547320758683e76de6428dadef06ae522
Kobayasi, Hiroaki <buribullet@gmail.com>
2022-10-29 21:09:15 +0900

    GH-70 - experimental support for nested json_type
    - Also introduced MOP4Import::Base::JSON for JSON-compat base object

--- Util/JSON_TYPE.pm
+++ Util/JSON_TYPE.pm
@@ -4,7 +4,9 @@ use warnings;
 
 use constant DEBUG => $ENV{DEBUG_MOP4IMPORT};
 
-use MOP4Import::Util qw/globref define_constant/;
+use MOP4Import::Util qw/globref define_constant terse_dump/;
+
+use MOP4Import::Opts qw/Opts/;
 
 use Cpanel::JSON::XS::Type;
 
@@ -31,17 +33,16 @@ BEGIN {
 
   foreach my $keyword (qw(hashof arrayof anyof null_or_anyof)) {
     my $longName = "json_type_$keyword";
-    *{globref(__PACKAGE__, $keyword)} = $CLS->can($longName);
+    my $sub = $CLS->can($longName);
+    *{globref(__PACKAGE__, $keyword)} = sub {
+      shift; $sub->(@_);
+    }
   }
 }
 
 sub intern_json_type {
-  my ($pack, $typeName) = @_;
-  if (ref $typeName) {
-    $typeName;
-  } else {
-    $JSON_TYPES{$typeName} //= $pack->build_json_type($typeName);
-  }
+  (my $pack, my Opts $opts, my $typeName) = @_;
+  $JSON_TYPES{$typeName} //= $pack->build_json_type($opts, $typeName);
 }
 
 sub lookup_json_type {
@@ -49,48 +50,94 @@ sub lookup_json_type {
   $JSON_TYPES{$typeName};
 }
 
+sub declare_json_type_record {
+  my ($pack, $typeName) = @_;
+  $JSON_TYPES{$typeName} //= +{};
+}
+
+sub inherit_json_type {
+  my ($pack, $typeName, $superType) = @_;
+  my $superSpec = $pack->lookup_json_type($superType)
+    or return;
+
+  my $thisSpec = $pack->declare_json_type_record($typeName);
+  print STDERR "  json_type $typeName inherits following items from $superType: "
+    , join(", ", sort keys %$superSpec), "\n" if DEBUG;
+
+  foreach my $key (keys %$superSpec) {
+    $thisSpec->{$key} = $superSpec->{$key};
+  }
+}
+
+sub resolve_json_type_in_context {
+  (my $pack, my Opts $opts, my $typeName) = @_;
+  Carp::confess "typename is undef!" unless defined $typeName;
+  if ($typeName =~ /^[A-Z]/ and $typeName !~ /::/
+      and my $sub = $opts->{destpkg}->can($typeName)) {
+    my $value = $sub->();
+    print STDERR ", $typeName is resolved to $value in $opts->{destpkg}"
+      if DEBUG;
+    $value;
+  } else {
+    print STDERR ", $typeName is used as-is in $opts->{destpkg}"
+      if DEBUG;
+    undef;
+  }
+}
+
 sub register_json_type_of_field {
-  my ($pack, $destpkg, $fieldName, $jsonType) = @_;
+  (my $pack, my Opts $opts, my ($destpkg, $fieldName, $jsonType)) = @_;
   my $typeRec = $JSON_TYPES{$destpkg} //= +{};
   unless (ref $typeRec eq 'HASH') {
     Carp::croak "Can't set json_type for $destpkg\->{$fieldName} because it was already declared as type: @{[$typeRec // '']}"
   }
 
-  my $found;
-  if (not ref $jsonType and ref ($found = $JSON_TYPES{$jsonType})) {
-    # If given $jsonType is a typename string and actual entry is a reference,
-    # we can weaken it.
-    $typeRec->{$fieldName} = $found;
-    Scalar::Util::weaken($typeRec->{$fieldName});
-  } else {
-    $typeRec->{$fieldName} = $pack->intern_json_type($jsonType);
-  }
+  push @{$opts->{delayed_tasks}}, sub {
+    print STDERR "  $fieldName json_type: spec=", terse_dump($jsonType)
+      if DEBUG;
+
+    my $typeName;
+    if (not ref $jsonType and $typeName = ($JSON_TYPES{$jsonType} || $pack->resolve_json_type_in_context($opts, $jsonType) || $jsonType) and my $found = $JSON_TYPES{$typeName}) {
+      $typeRec->{$fieldName} = $found;
+    } else {
+      $typeRec->{$fieldName} = $pack->intern_json_type($opts, $jsonType);
+    }
+    Scalar::Util::weaken($typeRec->{$fieldName}) if ref $typeRec->{$fieldName};
+
+    print STDERR "\n" if DEBUG;
+  };
 }
 
 
 sub build_json_type {
-  my ($pack, $typeSpec) = @_;
+  (my $pack, my Opts $opts, my $typeSpec) = @_;
   if (not defined $typeSpec) {
     Carp::croak "json_type is undef!";
   }
   elsif (not ref $typeSpec) {
-    if (defined (my $found = $JSON_TYPES{$typeSpec})) {
+    my $typeName = $pack->resolve_json_type_in_context($opts, $typeSpec) || $typeSpec;
+    if (defined (my $found = $JSON_TYPES{$typeName})) {
       # Note: weakening here does not take effect.
-      $found;
+      return $found;
     } elsif (my $sub = $pack->can(my $longName = "JSON_TYPE_".$typeSpec)) {
-      $sub->();
+      return $sub->();
     } else {
       Carp::croak "Unknown JSON_TYPE name: $typeSpec";
     }
   }
   elsif (ref $typeSpec eq 'ARRAY') {
     my ($keyword, @args) = @$typeSpec;
-    $pack->$keyword(map {$pack->build_json_type($_)} @args);
+    print STDERR "  build_json_type(", terse_dump($keyword, @args), ") => "
+      if DEBUG;
+    my @actual = map {$pack->build_json_type($opts, $_)} @args;
+    print STDERR " >> (", terse_dump($keyword, @actual), ")"
+      if DEBUG;
+    $pack->$keyword(@actual);
   }
   elsif (ref $typeSpec eq 'HASH') {
     my %spec;
     foreach my $key (keys %$typeSpec) {
-      $spec{$key} = $pack->build_json_type($typeSpec->{$key});
+      $spec{$key} = $pack->build_json_type($opts, $typeSpec->{$key});
     }
     \%spec;
   }
