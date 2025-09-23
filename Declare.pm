25e5657de866ecf14ebdf880f191dfc90cd743c6
Kobayasi, Hiroaki <buribullet@gmail.com>
2025-09-23 13:42:34 +0900

    GH-100 - use absolute path

--- Declare.pm
+++ Declare.pm
@@ -3,7 +3,7 @@ package MOP4Import::Declare;
 use 5.010;
 use strict;
 use warnings qw(FATAL all NONFATAL misc);
-our $VERSION = '0.070'; BEGIN {$VERSION = '0.062'};
+our $VERSION = '0.070_001'; BEGIN {$VERSION = '0.062'};
 use Carp;
 use mro qw/c3/;
 
